45fc2b28cc3dd726c96987539ca3d12bdb4e8812
박건우 <136959177+chaos1231107@users.noreply.github.com>
2023-10-01 02:43:07 +0900

    Add files via upload

--- vector_size_moving_avg_std.py
+++ vector_size_moving_avg_std.py
@@ -24,8 +24,8 @@ def moving_average(data, window):
         if x < window - 1:
             moving_avg.append(data[x])
         else:
-            partial_avg = np.mean(data[1-window+1 : x+1])
-            moving_avg.append([partial_avg])
+            partial_avg = np.mean(data[x-window+1 : x+1])
+            moving_avg.append(partial_avg)
     return moving_avg
 
 data = []
@@ -39,17 +39,17 @@ for j in range(1, TMS_data.shape[0]):
     relative_vector.append(result)
     data.append(result)
     ma = moving_average(data, window)  # moving average
- 
+    print(relative_vector)
     if j >= window and ma[j - window] > ma[j - window + 1]:
         # 이전 이동평균이 이후 이동평균의 수치보다 클때 값을 더이상 받지 않는다
         break
     
 
-time_step = range(1, len(relative_vector) + 1)
+time_stamp = range(1, len(relative_vector) + 1)
 
 # Plot the differences in vector sizes
-plt.plot(time_step, relative_vector, marker='o', linestyle='-')
-plt.xlabel('Time Step')
+plt.plot(time_stamp, relative_vector, marker='o', linestyle='-')
+plt.xlabel('Time Stamp')
 plt.ylabel('Difference in Vector Size')
 plt.title('Difference in Vector Size Over Time')
 plt.grid(True)
